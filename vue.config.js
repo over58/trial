c48a0edfc4c3f5d24712660df25ff2aae748632c
xuyongchao <xuyongchao6585@ipalfish.com>
2020-08-28 00:20:05 +0800

    update yml

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const isDev = process.env.NODE_ENV === 'development'
 
 module.exports = {
-  publicPath: isDev ? '' : '/trial/'
+  publicPath: isDev ? '' : '/trial/',
+  lintOnSave: true,
+  configureWebpack: config => {
+    config.devtool = isDev
+      ? 'cheap-module-eval-source-map'
+      : 'hidden-source-map'
+  }
 }
