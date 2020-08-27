7eec73f9ef5a25690fa883f731d93dd3537a8a54
over <26474133+over58@users.noreply.github.com>
2020-08-27 17:03:18 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import routes from './routes.js'
+import Conttaienr from '../App.vue'
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Container',
+      component: Conttaienr,
+      children: routes
+    }
+  ]
+})
---
+++ src/router/routes.js
@@ -0,0 +1,7 @@
+export default [
+  {
+    path: '',
+    name: 'Nav',
+    component: () => import(/* webpackChunkName */ '../views/Nav.vue')
+  }
+]
