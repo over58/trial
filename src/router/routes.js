9231feda4af3d99aecbeddfe07474054f0a5cdd6
over <26474133+over58@users.noreply.github.com>
2020-08-29 20:33:57 +0800

    feat: update article

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,35 +1,35 @@
 var routes = [
   {
     path: '',
+    redirect: 'trial/qrcode',
+    component: () => import('@views/npm/qrcode/Index.vue')
+  },
+  {
+    path: 'trial/qrcode',
     name: 'qrcode',
     component: () => import('@views/npm/qrcode/Index.vue')
   },
   {
-    path: 'clipboard',
+    path: 'trial/clipboard',
     name: 'clipboard',
     component: () => import('@views/npm/clipboard/Index.vue')
   },
   // css
   {
-    path: 'vertical-align',
+    path: 'trial/vertical-align',
     name: 'vertical-align',
     component: () => import('@views/css/vertical-align.vue')
   },
   {
-    path: 'sector',
+    path: 'trial/sector',
     name: 'sector',
     component: () => import('@views/css/sector.vue')
   },
   {
-    path: 'ease',
+    path: 'trial/ease',
     name: 'ease',
     component: () => import('@views/css/ease.vue')
   }
 ]
-
-routes.map(x => {
-  x.path = 'trial/' + x.path
-  return x
-})
 
 export default routes
