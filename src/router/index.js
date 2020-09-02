c7255b022f6b98fc73931598b6f53bfd941b14da
xuyongchao <xuyongchao6585@ipalfish.com>
2020-09-02 19:37:36 +0800

    feat: hash mode route

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import Container from '../views/Container.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  mode: 'hash',
   routes: [
     {
       path: '/',
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,32 +1,32 @@
 var routes = [
   {
     path: '',
-    redirect: 'trial/qrcode',
+    redirect: 'qrcode',
     component: () => import('@views/npm/qrcode/Index.vue')
   },
   {
-    path: 'trial/qrcode',
+    path: 'qrcode',
     name: 'qrcode',
     component: () => import('@views/npm/qrcode/Index.vue')
   },
   {
-    path: 'trial/clipboard',
+    path: 'clipboard',
     name: 'clipboard',
     component: () => import('@views/npm/clipboard/Index.vue')
   },
   // css
   {
-    path: 'trial/vertical-align',
+    path: 'vertical-align',
     name: 'vertical-align',
     component: () => import('@views/css/vertical-align.vue')
   },
   {
-    path: 'trial/sector',
+    path: 'sector',
     name: 'sector',
     component: () => import('@views/css/sector.vue')
   },
   {
-    path: 'trial/ease',
+    path: 'ease',
     name: 'ease',
     component: () => import('@views/css/ease.vue')
   },
