9be9f3dcaff77c01574ab45b1d022295caab1911
over58 <26474133+over58@users.noreply.github.com>
2021-04-29 08:41:10 +0000

    Deploying to gh-pages from  @ 100cff2ebcaba1e37e253039e5a0f9301f1d8f58 🚀

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import comment from '@/components/comment'
-import modal from '@/components/modal'
-
-Vue.component('comment', comment)
-Vue.component('modal', modal)
--- test/ts/namespace/b.ts
+++
@@ -1,3 +0,0 @@
- namespace G{
-    export const name = 'GGG'
-}
